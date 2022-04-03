7fa2db25a857065a8e8411b098d27fa21aa21480
NickyL279 <60162477+NickyL279@users.noreply.github.com>
2022-04-02 20:33:04 -0700

    a4-complete-v5

--- src/services/users-service.js
+++ src/services/users-service.js
@@ -1,16 +1,16 @@
 import axios from "axios";
-// const BASE_URL = "https://zliu-node-on-heroku.herokuapp.com/api";
-const BASE_URL = "http://localhost:4000/api";
+const BASE_URL = "https://zliu-node-on-heroku.herokuapp.com/api";
+//const BASE_URL = "http://localhost:4000/api";
 const USERS_API = `${BASE_URL}/users`;
 const LOGIN_API = `${BASE_URL}/login`;
 
 
 export const createUser = (user) =>
-  axios.post("http://localhost:4000/api/users", user)
+  axios.post(`${USERS_API}`, user)
     .then(response => response.data);
 
 export const findAllUsers = () =>
-    axios.get("http://localhost:4000/api/users")
+    axios.get(USERS_API)
         .then(response => response.data);
 
 export const findUserById = (uid) =>
